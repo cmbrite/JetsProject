89aa8efb67abc0a5bb86dfbc921264a68d5025fd
Chris Brite <chrismbrite@gmail.com>
2022-04-03 19:54:57 -0700

    Text file values updated

--- src/com/skilldistillery/jets/entities/SeaPlane.java
+++
@@ -1,9 +0,0 @@
-package com.skilldistillery.jets.entities;
-
-public class SeaPlane extends Jet{
-
-	public SeaPlane(String model, double speed, int range, long price) {
-		super(model, speed, range, price);
-	}
-
-}
